bb60b4a65278118554ee50e24b4d0d2f3843e2d3
jkkoh <jeongkyu7@gmail.com>
2021-11-25 13:39:03 +0900

    third commit

---
+++ src/components/aClick.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>welcome to bClick.vue</p>
+        <p>{{this.clickValue}} : 상위 about.vue 에서 받아온 값</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "ClickComponent",
+    props: ["clickValue"],
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>하위 컴포넌트로 데이터 값을 보내줍니다</p>
+    <ClickComponent v-bind:clickValue="aboutValue"/>
   </div>
 </template>
+
+<script>
+import ClickComponent from "@/components/aClick.vue";
+export default {
+  name: "AboutComponent",
+  components: { ClickComponent },
+  data (){
+    return{
+      aboutValue : 10,
+    };
+  },
+}
+</script>
